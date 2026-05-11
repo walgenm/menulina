41ac2f5c5a42d4531b9462cf3174d50c593ecbb1
Wall <moronwalgen@gmamil.com>
2026-05-11 15:35:02 -0400

    Hice todo el trabajo

---
+++ ejercicios/src/Walgen_Moron_funciones/Menu.java
@@ -0,0 +1,18 @@
+package Walgen_Moron_funciones;
+
+/*
+Trabajo de funciones
+Carrera: Ingenieria de sistemas
+Sección: 05S-2626-D1
+Alumnos: 
+    Diego Medina   C.I: 31934048
+    Walgen Morón   C.I: 31940641
+*/
+
+public class Menu {
+    
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+    
+}
